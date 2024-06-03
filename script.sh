5f0596477bf64a301b5315f05adc363f3cd41c44
Himemizu Yayoi <himemizuyayoi@Himemizus-MacBook-Pro.local>
2024-06-03 05:11:44 +0100

    update script

--- script.sh
+++ script.sh
@@ -1,16 +1,36 @@
-python main_fed.py --dataset mnist  --epochs 50  --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.2
-python main_fed.py --dataset mnist  --epochs 50  --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt Adam --lr 0.003
+python main_fed.py --dataset mnist  --epochs 50  --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.2 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+python main_fed.py --dataset mnist  --epochs 50  --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt Adam --lr 0.003 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
 
 
 
-python main_fed.py --dataset fmnist  --epochs 50  --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.2
-python main_fed.py --dataset fmnist  --epochs 50 --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt Adam --lr 0.003
+python main_fed.py --dataset fmnist  --epochs 50 --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.2 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+python main_fed.py --dataset fmnist  --epochs 50 --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt Adam --lr 0.003 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
 
 
 
-# python main_fed.py --dataset cifar  --epochs 50 --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.2
-# python main_fed.py --dataset cifar  --epochs 50 --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.001
+python main_fed.py --dataset cifar  --epochs 50 --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.2 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+python main_fed.py --dataset cifar  --epochs 50 --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.001 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
 
 
-# python main_fed.py --dataset SVHN  --epochs 50 --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.2
-# python main_fed.py --dataset SVHN  --epochs 50  --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.001
+python main_fed.py --dataset SVHN  --epochs 50 --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.2 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+python main_fed.py --dataset SVHN  --epochs 50 --local_ep 2 --local_bs 128 --gpu -1 --num_users 6 --bs 128 --opt SGD --lr 0.001 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+
+
+
+# If GPU
+# python main_fed.py --dataset mnist  --epochs 50  --local_ep 2 --local_bs 128 --gpu 0 --num_users 6 --bs 128 --opt SGD --lr 0.2 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+# python main_fed.py --dataset mnist  --epochs 50  --local_ep 2 --local_bs 128 --gpu 0 --num_users 6 --bs 128 --opt Adam --lr 0.003 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+
+
+
+# python main_fed.py --dataset fmnist  --epochs 50 --local_ep 2 --local_bs 128 --gpu 0 --num_users 6 --bs 128 --opt SGD --lr 0.2 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+# python main_fed.py --dataset fmnist  --epochs 50 --local_ep 2 --local_bs 128 --gpu 0 --num_users 6 --bs 128 --opt Adam --lr 0.003 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+
+
+
+# python main_fed.py --dataset cifar  --epochs 50 --local_ep 2 --local_bs 128 --gpu 0 --num_users 6 --bs 128 --opt SGD --lr 0.2 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+# python main_fed.py --dataset cifar  --epochs 50 --local_ep 2 --local_bs 128 --gpu 0 --num_users 6 --bs 128 --opt SGD --lr 0.001 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+
+
+# python main_fed.py --dataset SVHN  --epochs 50 --local_ep 2 --local_bs 128 --gpu 0 --num_users 6 --bs 128 --opt SGD --lr 0.2 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
+# python main_fed.py --dataset SVHN  --epochs 50 --local_ep 2 --local_bs 128 --gpu 0 --num_users 6 --bs 128 --opt SGD --lr 0.001 --HFL_Clients 8 --VFL_Clients 2 --HFL_Data_Split 0.8
